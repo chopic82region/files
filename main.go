f3aec1c825b58a8e52377868fd5604eb83b852f1
Sergej Zaporozhets <tataka18252@gmail.com>
2025-05-05 18:52:34 +0300

    need write merg channels function

--- main.go
+++ main.go
@@ -2,28 +2,34 @@ package main
 
 import (
 	"fmt"
-	"sync"
+	"time"
 )
 
+type emptyStruct struct{}
+
 func main(){
-	count := 10
-	wg := sync.WaitGroup{}
 
-	// какой то код
-	wg.Add(count)
-	go func(){
+	ch1 := make(chan interface{})
+	ch2 := make(chan interface{})
+	ch3 := make(chan interface{})
 	
-		for i := range count{
-			defer wg.Done()
-			fmt.Println("hello world", i)
-		}
-	}()
+	ch1 <- "Hello"
+	ch2 <- 1123
+	ch3 <- emptyStruct{} 
 
-
-	wg.Wait()
-
-	fmt.Println("end")
-	
-
+	fmt.Println(mergeChannels(ch1, ch2, ch3))
 
 }	
+
+func mergeChannels (ch ... chan interface{}) <- chan interface{}{
+	mergedCannel := make(<- chan interface{})
+
+	go func(){
+		select{
+		case <- time.After(5*time.Second):
+			fmt.Println("time out")
+		}
+	}()
+	
+	return mergedCannel
+}
